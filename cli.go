c0376310804b1a08a6292a0a1c53ffd53b4d204b
Layerex <layerex@dismail.de>
2024-08-14 16:42:49 +0300

    Allow exporting emoji sets

--- cli.go
+++ cli.go
@@ -38,7 +38,7 @@ type Args struct {
 }
 
 func ParseArgs() Args {
-	stickerSetURLRegex := regexp.MustCompile("^(?:http://|https://)?[^/]+/addstickers/([a-zA-Z0-9_]{5,32})/*")
+	stickerSetURLRegex := regexp.MustCompile("^(?:http://|https://)?[^/]+/add(?:stickers|emoji)/([a-zA-Z0-9_]{5,32})/*")
 	stickerSetNameRegex := regexp.MustCompile("[a-zA-Z0-9_]{5,32}")
 
 	var args Args
